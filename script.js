ab5273c226db22a3c84e3327e9655da7c4c9bd0e
valeriocipolla <valeriocipolla92@gmail.com>
2021-11-05 09:11:36 +0000

    refactored filter logic

--- script.js
+++ script.js
@@ -8,171 +8,70 @@ const scrollToAdd = 600;
 const bestRatedButton = document.getElementById("best-rated-btn");
 const comedyButton = document.getElementById("comedy-btn");
 const dramaButton = document.getElementById("drama-btn");
-const horroButton = document.getElementById("horror-btn");
+const horrorButton = document.getElementById("horror-btn");
 const displayedGenre = document.querySelector(".genre");
 let movieList = [];
 
-async function showHorrors() {
+async function showPickedCategory(event) {
   dropdownMenu.classList.toggle("invisible");
   carousel.innerHTML = "";
   movieList.splice(0, movieList.length);
+  let moviesToDisplay = [];
   for (let i = 1; i <= 10; i++) {
     let result = await fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${i}`
     )
       .then((response) => response.json())
       .then((data) => {
-        let movies = data.results.filter((movie) =>
-          movie.genre_ids.includes(27)
-        );
-        movies.map((movie) => {
-          movieList.push(
-            {
-              id: movie.id,
-              title: movie.title,
-              rating: movie.vote_average,
-              realeaseDate: movie.release_date,
-              description: movie.overview,
-             }
-            )
-          carousel.insertAdjacentHTML(
-            "beforeend",
-            `<img class="movie-img" 
-            data-id="${movie.id}"
-            src=
-          "https://image.tmdb.org/t/p/w200/${movie.poster_path}
-          "
-          />`
+        if (event.target === bestRatedButton) {
+          moviesToDisplay = data.results.filter(
+            (movie) => movie.vote_average >= 8
           );
-        });
-      });
-  }
-  displayedGenre.textContent = "Horrors";
-  let movieElements = document.querySelectorAll(".movie-img");
-  movieElements.forEach((movie) => {
-    movie.addEventListener("click", showModalHandler);
-  })
-}
-
-async function showDramas() {
-  dropdownMenu.classList.toggle("invisible");
-  carousel.innerHTML = "";
-  movieList.splice(0, movieList.length);
-  for (let i = 1; i <= 10; i++) {
-    let result = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${i}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        let movies = data.results.filter((movie) =>
-          movie.genre_ids.includes(18)
-        );
-        movies.map((movie) => {
-          movieList.push(
-            {
-              id: movie.id,
-              title: movie.title,
-              rating: movie.vote_average,
-              realeaseDate: movie.release_date,
-              description: movie.overview,
-             }
-            )
-          carousel.insertAdjacentHTML(
-            "beforeend",
-            `<img class="movie-img" 
-            data-id="${movie.id}"
-            src=
-          "https://image.tmdb.org/t/p/w200/${movie.poster_path}
-          "
-          />`
+        } else if (event.target === comedyButton) {
+          moviesToDisplay = data.results.filter((movie) =>
+            movie.genre_ids.includes(35)
           );
-        });
-      });
-  }
-  displayedGenre.textContent = "Dramas";
-  let movieElements = document.querySelectorAll(".movie-img");
-  movieElements.forEach((movie) => {
-    movie.addEventListener("click", showModalHandler);
-  })
-}
-
-async function showComedies() {
-  dropdownMenu.classList.toggle("invisible");
-  carousel.innerHTML = "";
-  movieList.splice(0, movieList.length);
-  for (let i = 1; i <= 10; i++) {
-    let result = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${i}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        let movies = data.results.filter((movie) =>
-          movie.genre_ids.includes(35)
-        );
-        movies.map((movie) => {
-          movieList.push(
-            {
-              id: movie.id,
-              title: movie.title,
-              rating: movie.vote_average,
-              realeaseDate: movie.release_date,
-              description: movie.overview,
-             }
-            )
-          carousel.insertAdjacentHTML(
-            "beforeend",
-            `<img class="movie-img" 
-            data-id="${movie.id}"
-            src=
-          "https://image.tmdb.org/t/p/w200/${movie.poster_path}
-          "
-          />`
+        } else if (event.target === dramaButton) {
+          moviesToDisplay = data.results.filter((movie) =>
+            movie.genre_ids.includes(18)
           );
-        });
-      });
-  }
-  displayedGenre.textContent = "Comedies";
-  let movieElements = document.querySelectorAll(".movie-img");
-  movieElements.forEach((movie) => {
-    movie.addEventListener("click", showModalHandler);
-  })
-}
-
-async function showBestRated() {
-  dropdownMenu.classList.toggle("invisible");
-  carousel.innerHTML = "";
-  movieList.splice(0, movieList.length);
-  let result = await fetch(
-    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      data.results.map((movie) => {
-        movieList.push(
-          {
+        } else if (event.target === horrorButton) {
+          moviesToDisplay = data.results.filter((movie) =>
+            movie.genre_ids.includes(27)
+          );
+        }
+        moviesToDisplay.map((movie) => {
+          movieList.push({
             id: movie.id,
             title: movie.title,
             rating: movie.vote_average,
             realeaseDate: movie.release_date,
             description: movie.overview,
-           }
-          )
-        carousel.insertAdjacentHTML(
-          "beforeend",
-          `<img class="movie-img" 
-          data-id="${movie.id}"
-          src=
+          });
+          carousel.insertAdjacentHTML(
+            "beforeend",
+            `<img class="movie-img" 
+            data-id="${movie.id}"
+            src=
           "https://image.tmdb.org/t/p/w200/${movie.poster_path}
-          " 
+          "
           />`
-        );
+          );
+        });
       });
-    });
-  displayedGenre.textContent = "Best Rated";
+  }
+  if (event.target === bestRatedButton)
+    displayedGenre.textContent = "Best rated";
+  else if (event.target === comedyButton)
+    displayedGenre.textContent = "Comedies";
+  else if (event.target === dramaButton) 
+    displayedGenre.textContent = "Dramas";
+  else if (event.target === horrorButton)
+    displayedGenre.textContent = "Horrors";
   let movieElements = document.querySelectorAll(".movie-img");
   movieElements.forEach((movie) => {
     movie.addEventListener("click", showModalHandler);
-  })
+  });
 }
 
 async function showMovieData() {
@@ -182,15 +81,13 @@ async function showMovieData() {
     .then((response) => response.json())
     .then((data) => {
       data.results.map((movie) => {
-        movieList.push(
-          {
-            id: movie.id,
-            title: movie.title,
-            rating: movie.vote_average,
-            realeaseDate: movie.release_date,
-            description: movie.overview,
-           }
-          )
+        movieList.push({
+          id: movie.id,
+          title: movie.title,
+          rating: movie.vote_average,
+          realeaseDate: movie.release_date,
+          description: movie.overview,
+        });
         carousel.insertAdjacentHTML(
           "beforeend",
           `<img class="movie-img" 
@@ -205,12 +102,13 @@ async function showMovieData() {
   let movieElements = document.querySelectorAll(".movie-img");
   movieElements.forEach((movie) => {
     movie.addEventListener("click", showModalHandler);
-  })
-  
+  });
 }
 
 async function showModalHandler() {
-  let targetMovie = movieList.find(movie => movie.id == event.target.getAttribute("data-id"));
+  let targetMovie = movieList.find(
+    (movie) => movie.id == event.target.getAttribute("data-id")
+  );
   event.target.style.pointerEvents = "none";
   let modalBackground = document.createElement("div");
   modalBackground.className = "modal-bg";
@@ -218,14 +116,17 @@ async function showModalHandler() {
   modal.className = "modal-box";
   modal.innerHTML = `
   <h1>${targetMovie.title}</h1>
-  <p class="modal-footer">Realeased in ${targetMovie.realeaseDate.slice(0, 4)} <span>Rating: ${targetMovie.rating}/10</span></p>
+  <p class="modal-footer">Realeased in ${targetMovie.realeaseDate.slice(
+    0,
+    4
+  )} <span>Rating: ${targetMovie.rating}/10</span></p>
   <p class="modal-desc">${targetMovie.description}</p>
   <p class ="modal-buttons"><button>Watch</button><button>Close</button></p>
   `;
   document.body.append(modalBackground);
   modalBackground.appendChild(modal);
   let closeButton = document.querySelector(".modal-buttons").lastElementChild;
-  closeButton.addEventListener("click", removeModalHandler)
+  closeButton.addEventListener("click", removeModalHandler);
 }
 
 function removeModalHandler() {
@@ -265,13 +166,13 @@ scrollLeftButton.addEventListener("click", scrollLeftHandler);
 
 categoriesButton.addEventListener("click", dropdownHandler);
 
-bestRatedButton.addEventListener("click", showBestRated);
+bestRatedButton.addEventListener("click", showPickedCategory);
 
-comedyButton.addEventListener("click", showComedies);
+comedyButton.addEventListener("click", showPickedCategory);
 
-dramaButton.addEventListener("click", showDramas);
+dramaButton.addEventListener("click", showPickedCategory);
 
-horroButton.addEventListener("click", showHorrors);
+horrorButton.addEventListener("click", showPickedCategory);
 
 window.addEventListener("resize", resizeHandler);
 
